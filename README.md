881d43ebd67fe2e21ea09eac2d5468f775e2b7d6
Aleksei Verbitski <leshaverbitsky@gmail.com>
2024-11-25 11:25:11 +0100

    feat: create new firebase project, add secrets

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # enduro_diagnostics_map
 
 ## TODO
-1. Add splash screen
-2. Add Telegram login
-3. Add BD
+1. Add Telegram login
+2. Add BD
+3. Add Default mechanic name
+4. Add possibility to edit diagnostics
+5. Add diagnostics inheritance
--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
   await Firebase.initializeApp(
+    name: 'enduro_diagnostics_map',
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
